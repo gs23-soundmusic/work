418678223cdedc52262b43a0adffa0bb9989db70
gss23 <gs23@pdx.edu>
2026-04-15 12:51:05 -0700

    progress

--- clipped/app.py
+++ clipped/app.py
@@ -2,11 +2,21 @@ import numpy as np
 from scipy.io import wavfile
 import sounddevice as sd
 
-NSECS = 3
+NSECS = 1
 SAMPLE_RATE = 48_000
 NSAMPLES = NSECS * SAMPLE_RATE
-F = 440
+FREQUENCY = 440
+MAX_AMP = 32767
+HALF_AMP = MAX_AMP // 2
+QUARTER_AMP = MAX_AMP // 4
 
 t = np.linspace(0, NSECS, num=NSAMPLES, endpoint=False, dtype=np.float32)
-f = np.sin(2 * np.pi * F * t) * t / NSECS
-wavfile.write('sine.wav', SAMPLE_RATE, f)
+
+sine_wave = np.sin(2 * np.pi * FREQUENCY * t) * QUARTER_AMP
+since_wave = sine_wave.astype(np.int16)
+wavfile.write('sine.wav', SAMPLE_RATE, sine_wave)
+
+clipped_wave = HALF_AMP * np.sin(2 * np.pi * FREQUENCY * t)
+clipped_wave = np.clip(clipped_wave, -QUARTER_AMP, QUARTER_AMP)
+clipped_wave = clipped_wave.astype(np.int16)
+wavfile.write('clipped.wav', SAMPLE_RATE, clipped_wave)
