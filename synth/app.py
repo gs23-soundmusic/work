e61d84ac8444a4bffb382a34019457c264a998b2
gssoto03 <gabriel.s.soto@outlook.com>
2026-06-05 08:41:47 -0700

    volume

--- synth/app.py
+++ synth/app.py
@@ -14,18 +14,18 @@ DURATION = 0.01
 
 
 #We will use the now-familiar sawtooth wave as our synthesizer sound. Run it at -3dBFS: about 0.708 of full scale.
-def waveforms(note, duration, type):
+def waveforms(note, duration, type, volume):
     frequency = 440 * (2 ** ((note - 69) / 12))
     t = np.linspace(0, duration, int(SAMPLE_RATE * duration))
     
     if type == 'sawtooth':
-        return WAVE_SCALE * (2 * (t % (1 / frequency)) - 1)
+        return WAVE_SCALE * volume * (2 * (t % (1 / frequency)) - 1)
     elif type == 'sine':
-        return WAVE_SCALE * np.sin(t * (2 * np.pi * frequency))
+        return WAVE_SCALE * volume * np.sin(t * (2 * np.pi * frequency))
     elif type == 'triangle':
-        return WAVE_SCALE * np.abs(np.sin(t * (2 * np.pi * frequency)))
+        return WAVE_SCALE * volume * np.abs(np.sin(t * (2 * np.pi * frequency)))
     elif type == 'square':
-        return WAVE_SCALE * np.sign(np.sin(t * (2 * np.pi * frequency)))
+        return WAVE_SCALE * volume * np.sign(np.sin(t * (2 * np.pi * frequency)))
 
 
 #Put a fixed attack-release volume envelope on the sawtooth. Start a note by ramping up linearly in amplitude from zero to full scale over 10 milliseconds. End the note by ramping down to zero over 10 milliseconds.
@@ -39,11 +39,13 @@ def ar_envelope(wave, attack=DURATION, release=DURATION):
 
 #Drive the synthesizer with a MIDI file. Send the sound to your system's default output as usual.
 def midi_events(in_midi, wave_type):
+    volume = 1
     for msg in in_midi:
         if msg.time > 0:
-            print(msg)
+            pass
+            #print(msg)
         if msg.type == 'note_off' and msg.time > 0:
-            wave = waveforms(msg.note, msg.time, wave_type)
+            wave = waveforms(msg.note, msg.time, wave_type, volume)
             envelope = ar_envelope(wave)
             sd.play(wave * envelope, SAMPLE_RATE)
             time.sleep(msg.time)
@@ -51,11 +53,16 @@ def midi_events(in_midi, wave_type):
         elif msg.type == 'note_on' and msg.time > 0:
             time.sleep(msg.time)
     
+        #MIDI KEY ON Velocity: Set the volume of the played note according to the KEY ON velocity.
+        elif msg.type == 'note_on' and msg.velocity > 0:
+            volume = msg.velocity / 127
+
         elif msg.type == 'note_on' or msg.type == 'note_off' and msg.velocity == 0:
             pass
 
 
 
+#Alternate Waveforms (--sine, --square, --triangle, etc): Allow performing with other voices.
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'wst', ['sine', 'square', 'triangle'])
 except getopt.GetoptError as err:
